47a38a5f1892f0d8243b44d1d81b81b58b92b738
georgikoichev <georgikoichev445@gmail.com>
2023-12-19 00:02:31 +0200

    Added styles

--- client/src/components/core/Home.jsx
+++ client/src/components/core/Home.jsx
@@ -1,7 +1,14 @@
+import { Link } from 'react-router-dom';
+
 export default function Home() {
     return (
         <section>
-            <h1>Home page</h1>  
+            <h1>Home page</h1>
+            <div className="home-description">
+                This is the home page for this application. Go to
+                <Link to="/posts">All Posts</Link>
+                see all created posts.
+            </div>
         </section>
     );
 }
